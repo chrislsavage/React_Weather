4c0ac79fdab3f812b7dd2ff0f13109934d6b7203
Chris Savage <chrislsavage@gmail.com>
2016-02-22 11:41:49 -0500

    Add Sparklines and Chart Component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
-export default class WeatherList extends Component {
+export class WeatherList extends Component {
+
+  renderWeather(cityData){
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.temp);
+    const humidity = cityData.list.map(weather => weather.main.temp);
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+      </tr>
+    )
+  }
   render() {
     return (
       <table className="table table-hover">
           <thead>
             <tr>
               <th> City </th>
+              <th> Temperature </th>
+              <th> Pressure </th>
+              <th> Humidity </th>
             </tr>
           </thead>
           <tbody>
+            {this.props.weather.map(this.renderWeather)}
           </tbody>
         </table>
     );
   }
 }
+
+function mapStateToProps({weather}) {
+  return { weather}; // {weather} == {weather: weather}
+}
+
+export default connect(mapStateToProps)(WeatherList);
